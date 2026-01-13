828644df375d7406708514a1a089466f1c26c506
Sarah Kervran <sarahkervran@outlook.fr>
2026-01-13 18:41:40 +0100

    nouveau test 2

--- test_file.py
+++ test_file.py
@@ -1,17 +1,17 @@
 import pytest
 
 def test_calc_addition():
-    output = 2+4
+    output = 2 + 4
     assert output == 6
 
 def test_calc_substraction():
   # Fonction test du résultat de 2-4
-    output = 2-4
+    output = 2 - 4
     assert output == -2
 
 def test_calc_multiply():
   # Fonction test du résultat de 2*4
-    output = 2*4
+    output = 2 * 4
     assert output == 8
 
 def test_coucou():
